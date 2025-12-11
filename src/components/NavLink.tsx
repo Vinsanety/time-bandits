4f393d2002a952a82c49be12eb5b37a39a85f58f
Vince Vahala <vincevahala@gmail.com>
2025-12-11 09:39:01 -0700

    Styled HeaderNav and NavLink. Added scroll styles to HeaderNav.

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -21,13 +21,13 @@ export function NavLink({
   const isActive = pathname === href;
 
   const base =
-    "block w-fit px-3 py-2 text-md font-medium transition-all duration-200";
+    "block w-fit px-3 py-2 text-md font-medium transition-all duration-200 hover:border-blue-600 hover:text-blue-600 focus-visible:bg-blue-100 focus-visible:outline-none focus-visible:border-blue-600";
 
   const styles = cn(
     base,
     isActive || variant === "active"
       ? "border-b-2 border-blue-600"
-      : "border-b-2 border-transparent text-gray-700 hover:border-blue-600 hover:text-blue-600 hover:bg-gray-100",
+      : "border-b-2 border-transparent text-gray-700",
     className
   );
 
